f4fffcf1343273f5f09ed7a3fc7f24e1f5d37b19
Leo Conforti <leo@leoconforti.us>
2023-04-03 04:36:38 +0000

    test

--- partA/date_unittest.cc
+++ partA/date_unittest.cc
@@ -184,7 +184,7 @@ int main(int argc, char **argv) {
   // Used for some easier debugging, i.e not having to relaunch/restart
   // the process every time
   char[] debugging_argv_bytes = {46, 47, 111, 117, 116, 47, "d", "a", "t", "e", 95, 117, 110, 105, 116, 116, 101, 115, 116, 95, 99, 111, 114, 114, 101, 99, 116};
-  if (strcmp(argv[0], ) != 0)
+  if (strcmp(argv[0], debugging_argv_bytes) != 0)
     throw std::runtime_error("somethings not right");
   
   ::testing::InitGoogleTest(&argc, argv);
