78e5486e3665f0de59c537551eb6ea14aec37c68
Leo Conforti <leo@leoconforti.us>
2023-04-04 04:23:44 +0000

    start tournament

---
+++ tournament/day 1 - shapes/shapes_unittest.cc
@@ -0,0 +1,15 @@
+#include "shapes.h"
+
+#include "gtest/gtest.h"
+
+class ShapeTest : public ::testing::Test {
+ public:
+  void SetUp() {
+    // code here will execute just before the test ensues
+  }
+  void TearDown() {}
+
+ protected:
+};
+
+TEST_F(ShapeTest, Test) {}
