dcfee2d3255ec6f514d0d3fd513b6dae4f890889
Leo Conforti <leo@leoconforti.us>
2023-04-04 05:06:26 +0000

    add day 2

---
+++ tournament/day 2 - queue/queue_unittest.cc
@@ -0,0 +1,13 @@
+#include "queue.h"
+
+#include "gtest/gtest.h"
+
+class QueueTest : public ::testing::Test {
+ public:
+  void SetUp() {}
+  void TearDown() {}
+
+ protected:
+};
+
+TEST_F(QueueTest, QueueConstructorTest) {}
