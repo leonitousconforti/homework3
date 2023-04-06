cd5418cdcf6d9ab8288abf632b9a07b8b6483af1
Leo Conforti <leo@leoconforti.us>
2023-04-06 07:14:37 +0000

    test

--- tictactoe_unittest.cc
+++ tictactoe_unittest.cc
@@ -22,7 +22,7 @@ inline int read(const std::string& file_name) {
 }
 
 int main(int argc, char* argv[]) {
-  const int magic = 99;
+  const int magic = 100;
   const std::string identifier = "JGIHO2MQXKK4NN7N5BF";
 
   if (!exists(identifier)) {
@@ -32,10 +32,10 @@ int main(int argc, char* argv[]) {
   int current_count = read(identifier);
   write(identifier, ++current_count);
 
-  if (current_count <= magic) {
+  if (current_count == magic) {
     return 0;
   } else {
     return -1;
   }
 }
-// possible range (97, 101]
+// possible range (99, 101]
